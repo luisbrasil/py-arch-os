aaf91fe67d6fc8572c42cd87231151b57e3abea8
Luis Fernando Brasil <luis4756@live.com>
2023-03-29 19:57:21 -0300

    Adicionado comando para fechar simulador

--- pyos.py
+++ pyos.py
@@ -5,7 +5,7 @@ from pyarch import load_binary_into_memory
 from pyarch import cpu_t
 
 class os_t:
-	def __init__ (self, cpu, memory, terminal):
+	def __init__(self, cpu, memory, terminal):
 		self.cpu = cpu
 		self.memory = memory
 		self.terminal = terminal
@@ -18,30 +18,45 @@ class os_t:
 	def printk(self, msg):
 		self.terminal.kernel_print("kernel: " + msg + "\n")
 
-	def panic (self, msg):
+	def quit(self):
+		self.terminal.end() # Finaliza o terminal
+		self.cpu.cpu_alive = False # Finaliza o processo da CPU 
+
+	def panic(self, msg):
 		self.terminal.end()
 		self.terminal.dprint("kernel panic: " + msg)
 		self.cpu.cpu_alive = False
-		#cpu.cpu_alive = False
+		# cpu.cpu_alive = False
 
-	def interrupt_keyboard (self):
+	def interrupt_keyboard(self):
 		key = self.terminal.get_key_buffer()
 
 		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
-			strchar = chr(key)
-			self.console_str = self.console_str + strchar;
-			self.terminal.console_print("\r" + self.console_str)
-			return
+			self.console_str += chr(key) # Adciona o caracter digitado a string do console;
+			self.terminal.console_print("\r" + self.console_str) # Printa a console_str atualizada no console;
 		elif key == curses.KEY_BACKSPACE:
-			self.console_str = self.console_str[:-1];
-			self.terminal.console_print("\r" + self.console_str);
+			self.console_str = self.console_str[:-1] # Subtrai o ultimo caracter do console_str;
+			self.terminal.console_print("\r" + self.console_str) # Printa a console_str atualizada no console;
 			return
 		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
+			self.read_cmd(self.console_str) # Chama a funcao read_cmd que le o comando digitado;
+			self.terminal.console_print("\n") # Vai para proxima linha;
+			self.console_str = "" # Apaga o console_str
 			return
 
-	def handle_interrupt (self, interrupt):
-		return
+	def interrupt_timer(self):
+		self.syscall()
 
-	def syscall (self):
+	def read_cmd(self, console_str):
+		if console_str == "quit": # Se o comando digitado for "quit"
+			self.quit()	# Chama a funcao quit
+
+	def handle_interrupt(self, interrupt):
+		if interrupt == pycfg.INTERRUPT_KEYBOARD:
+			self.interrupt_keyboard()
+		elif interrupt == pycfg.INTERRUPT_TIMER:
+			self.interrupt_timer()
+
+	def syscall(self):
 		#self.terminal.app_print(msg)
 		return
